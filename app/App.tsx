312078719ad473aeefb75af24f6747986e2ea5dc
Pankaj Singh <pankaj@instnt.org>
2024-04-12 14:21:33 +0530

    Remove all the link which is point to bc gov from setting screen

--- app/App.tsx
+++ app/App.tsx
@@ -66,68 +66,68 @@ const App = () => {
   const { navigate } = useNavigation()
   const toggleSurveyVisibility = () => setSurveyVisible(!surveyVisible)
 
-  const helpLink = 'https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet/help'
+  //const helpLink = 'https://www2.gov.bc.ca/gov/content/governments/government-id/bc-wallet/help'
 
   if (!isTablet()) {
     Orientation.lockToPortrait()
   }
 
-  const settings = [
-    {
-      header: {
-        title: t('Settings.Help'),
-        icon: { name: 'help' },
-      },
-      data: [
-        {
-          title: t('Settings.HelpUsingBCWallet'),
-          accessibilityLabel: t('Settings.HelpUsingBCWallet'),
-          testID: testIdWithKey('HelpUsingBCWallet'),
-          onPress: () => Linking.openURL(helpLink),
-        },
-        {
-          title: t('Settings.GiveFeedback'),
-          accessibilityLabel: t('Settings.GiveFeedback'),
-          testID: testIdWithKey('GiveFeedback'),
-          onPress: toggleSurveyVisibility,
-        },
-        {
-          title: t('Settings.ReportAProblem'),
-          accessibilityLabel: t('Settings.ReportAProblem'),
-          testID: testIdWithKey('ReportAProblem'),
-          onPress: toggleSurveyVisibility,
-        },
-      ],
-    },
-    {
-      header: {
-        title: t('Settings.MoreInformation'),
-        icon: { name: 'info' },
-      },
-      data: [
-        {
-          title: t('Settings.TermsOfUse'),
-          accessibilityLabel: t('Settings.TermsOfUse'),
-          testID: testIdWithKey('TermsOfUse'),
-          onPress: () => navigate(Stacks.SettingStack as never, { screen: Screens.Terms } as never),
-        },
-        {
-          title: t('Settings.IntroductionToTheApp'),
-          accessibilityLabel: t('Settings.IntroductionToTheApp'),
-          testID: testIdWithKey('IntroductionToTheApp'),
-          onPress: () => navigate(Stacks.SettingStack as never, { screen: Screens.Onboarding } as never),
-        },
-        {
-          title: t('Settings.PlayWithBCWallet'),
-          accessibilityLabel: t('Settings.PlayWithBCWallet'),
-          testID: testIdWithKey('PlayWithBCWallet'),
-          onPress: () => Linking.openURL('https://digital.gov.bc.ca/digital-trust/showcase/'),
-        },
-      ],
-    },
-  ]
+  // const settings = [
+  //   {
+  //     header: {
+  //       title: t('Settings.Help'),
+  //       icon: { name: 'help' },
+  //     },
+  //     data: [
+  //       {
+  //         title: t('Settings.HelpUsingBCWallet'),
+  //         accessibilityLabel: t('Settings.HelpUsingBCWallet'),
+  //         testID: testIdWithKey('HelpUsingBCWallet'),
+  //         onPress: () => Linking.openURL(helpLink),
+  //       },
+  //       // {
+  //       //   title: t('Settings.GiveFeedback'),
+  //       //   accessibilityLabel: t('Settings.GiveFeedback'),
+  //       //   testID: testIdWithKey('GiveFeedback'),
+  //       //   onPress: toggleSurveyVisibility,
+  //       // },
+  //       {
+  //         title: t('Settings.ReportAProblem'),
+  //         accessibilityLabel: t('Settings.ReportAProblem'),
+  //         testID: testIdWithKey('ReportAProblem'),
+  //         onPress: toggleSurveyVisibility,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     header: {
+  //       title: t('Settings.MoreInformation'),
+  //       icon: { name: 'info' },
+  //     },
+  //     data: [
+  //       {
+  //         title: t('Settings.TermsOfUse'),
+  //         accessibilityLabel: t('Settings.TermsOfUse'),
+  //         testID: testIdWithKey('TermsOfUse'),
+  //         onPress: () => navigate(Stacks.SettingStack as never, { screen: Screens.Terms } as never),
+  //       },
+  //       {
+  //         title: t('Settings.IntroductionToTheApp'),
+  //         accessibilityLabel: t('Settings.IntroductionToTheApp'),
+  //         testID: testIdWithKey('IntroductionToTheApp'),
+  //         onPress: () => navigate(Stacks.SettingStack as never, { screen: Screens.Onboarding } as never),
+  //       },
+  //       {
+  //         title: t('Settings.PlayWithBCWallet'),
+  //         accessibilityLabel: t('Settings.PlayWithBCWallet'),
+  //         testID: testIdWithKey('PlayWithBCWallet'),
+  //         onPress: () => Linking.openURL('https://digital.gov.bc.ca/digital-trust/showcase/'),
+  //       },
+  //     ],
+  //   },
+  // ]
 
-  configuration.settings = settings
+  //configuration.settings = settings
 
   useEffect(() => {
     // Hide the native splash / loading screen so that our
